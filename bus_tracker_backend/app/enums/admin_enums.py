ace7f7eaff75cdafae56d78f4964244fb67ef4b7
delowarhossain84 <mddelowarhossainhda@gmail.com>
2026-09-15 15:54:17 +0600

    Setup Firebase App Distribution

--- bus_tracker_backend/app/enums/admin_enums.py
+++
@@ -1,6 +0,0 @@
-from enum import Enum
-
-class BusFilterStatus(str, Enum):
-    ACTIVE = "active"
-    INACTIVE = "inactive"
-    RUNNING = "running"
--- bus_tracker_backend/app/enums/bus_enums.py
+++
@@ -1,6 +0,0 @@
-from enum import Enum
-
-class TripStatus(str, Enum):
-    RUNNING = "running"
-    ENDED = "ended"
-    
